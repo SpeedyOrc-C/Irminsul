44ac6424d50547f7033ea7429eb1b2e8046ad478
陈湛明 <1670732206@qq.com>
2023-04-14 12:10:19 +0100

    Improve coordinate's appearance

--- irminsul-client/src/lib/relation-graph/Coordinate.svelte
+++ irminsul-client/src/lib/relation-graph/Coordinate.svelte
@@ -6,12 +6,12 @@
 </script>
 
 <div class="coordinate">
-    <div>x= {coordinate.x}</div>
-    <div>y= {coordinate.y}</div>
+    <div>x: {coordinate.x}</div>
+    <div>y: {coordinate.y}</div>
     <!-- Only cluster has size, atom does not have a size. -->
     {#if size != null}
-        <div>w= {size.x}</div>
-        <div>h= {size.y}</div>
+        <div>w: {size.x}</div>
+        <div>h: {size.y}</div>
     {/if}
 </div>
 
@@ -19,22 +19,22 @@
     .coordinate {
         position: absolute;
         transform: translate(-50%, 0);
-
+        
         bottom: 2rem;
-
+        width: 4rem;
+        padding: 0.2rem 0.4rem;
         border-radius: 0.2rem;
         border: 1px solid #444;
 
-        padding: 0.2rem 0.4rem;
-        width: 6rem;
         background-color: #eee;
         color: #222;
 
         z-index: 100000;
 
+        font-size: 0.7rem;
+
         & > div {
             font-family: fira-code-regular, monospace;
         }
     }
-
 </style>

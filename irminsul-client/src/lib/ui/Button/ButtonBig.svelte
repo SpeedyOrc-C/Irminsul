71752074360ee175248ccc8dbc73fbaf7e78575c
陈湛明 <1670732206@qq.com>
2023-04-24 16:28:50 +0100

    Add a new dialog

---
+++ irminsul-client/src/lib/ui/Button/ButtonBig.svelte
@@ -0,0 +1,19 @@
+<script>
+    import Button from "../Button.svelte";
+
+</script>
+
+<Button height="3rem" style="min-width: 15rem" on:button-clicked>
+    <div class="content font-hywh-85w"><slot /></div>
+</Button>
+
+<style>
+    .content {
+        line-height: 3rem;
+        padding: 0 1.1rem;
+        text-align: center;
+        
+        color: #3b4255;
+        font-size: 1.3rem;
+    }
+</style>

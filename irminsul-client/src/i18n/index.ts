f5a59245983a48a9c00d4c5013e1a4651b1fbc50
陈湛明 <chenzhanming04@foxmail.com>
2023-08-29 16:54:39 +0800

    Update i18n

--- irminsul-client/src/i18n/index.ts
+++ irminsul-client/src/i18n/index.ts
@@ -1,11 +1,12 @@
 import { init, register } from 'svelte-i18n'
 
-const defaultLocale = 'zh-cn'
+const defaultLocale = 'en-us'
 
-register('en-us', () => import('./locales/en-us.json'))
-register('zh-cn', () => import('./locales/zh-cn.json'))
+register('en-us', () => import('./locales/en-us.json'));
+register('zh-cn', () => import('./locales/zh-cn.json'));
+register('ja', () => import('./locales/ja.json'));
 
 init({
 	fallbackLocale: defaultLocale,
 	initialLocale: defaultLocale,
-})
+});

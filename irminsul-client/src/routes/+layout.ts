626a9ce5c1b8ee055787a4a59c08b1da0c26037e
陈湛明 <chenzhanming04@foxmail.com>
2023-08-30 10:32:05 +0800

    Fix: I18n error in build

--- irminsul-client/src/routes/+layout.ts
+++ irminsul-client/src/routes/+layout.ts
@@ -1,10 +1,4 @@
-import type { LayoutLoad } from "./$types";
-import { locale } from "svelte-i18n";
 import "../i18n/index";
 
 export const prerender = true;
 export const trailingSlash = 'always';
-
-export const load: LayoutLoad = async () => {
-    locale.set('zh-cn')
-}

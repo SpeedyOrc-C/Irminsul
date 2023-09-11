47dbf7a651e5235987e249bc1ba8a606f27aa5c7
陈湛明 <chenzhanming04@foxmail.com>
2023-09-11 13:44:07 +0800

    Update relation data

--- irminsul-api/app/Root/Teyvat/Inazuma/TriCommission/YashiroCommission.hs
+++ irminsul-api/app/Root/Teyvat/Inazuma/TriCommission/YashiroCommission.hs
@@ -5,19 +5,23 @@ module Root.Teyvat.Inazuma.TriCommission.YashiroCommission where
 import Irminsul
 import CommonRelations
 
+import Root.Teyvat.Mondstadt
+
 ayakaKamisato = "AyakaKamisato"
 ayatoKamisato = "AyatoKamisato"
 
 yashiroCommission = clusterLeaf "YashiroCommission" Organization
     [
         ayakaKamisato,
-        ayatoKamisato
+        ayatoKamisato,
+        thoma
     ]
     [
         ayakaKamisato `youngerSister` ayatoKamisato,
         ayatoKamisato `elderBrother` ayakaKamisato
     ]
     (layout (0, 15) [
-        al ayakaKamisato (-15, 0),
-        al ayatoKamisato (15, 0)
+        al ayakaKamisato (-7.5, 0),
+        al ayatoKamisato (7.5, 0),
+        al thoma (0, -15)
     ])
